88ef4d5a266eec39d6052e7be57768d2753570c1
srividya1004 <gmsrividya102004@gmail.com>
2026-01-30 15:41:41 +0530

    Add files via upload

---
+++ code14.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class code14
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int N;
+        System.out.println("Enter the N value:\n");
+        N = sc.nextInt();
+
+        int[] arr = new int[N];
+        for(int i=0;i<N;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<N;i++)
+        {
+           sum =sum+arr[i];
+        }
+         System.out.println("sum\n"+sum); 
+
+    
+      
+    }
+}
