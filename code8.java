7e6268c6d831f5a9e3792d91b7ef6ba00c064a82
srividya1004 <gmsrividya102004@gmail.com>
2026-01-30 13:56:56 +0530

    Add files via upload

---
+++ code8.java
@@ -0,0 +1,17 @@
+class car{
+    String color;
+    void run()
+    {
+        System.out.println("speed");
+    }
+}
+class code8
+{
+    public static void main(String[]args)
+    {
+        car RR = new car();
+        RR.color="white and black";
+        RR.run();
+    
+    }
+}
